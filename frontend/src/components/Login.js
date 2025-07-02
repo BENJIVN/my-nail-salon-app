b6afc9950af234bb579986d3f9262a2aec59c701
BENJIVN <bengi3288@gmail.com>
2025-07-02 02:58:36 -0400

    front-end: login page

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,51 @@
+//all necessary elements for user authentication
+import React, {useState} from 'react';
+import {Form, Button, Container, Row, Col} from 'react-bootstrap';
+
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Login attempet with:', {email, password});
+        //Send a requst to the server here with tokens
+
+    }
+    return (
+        <Container>
+            <Row className='justify-content-md-center mt-10'>
+                <Col xs={12} md={6}>
+                    <h2 className="text-center mb-4">Login</h2>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group className="mb-5" controlId="formBasicEmail">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Button variant ="primary" type="submit" className="btn btn-primary btn-sm w-100">
+                            Login
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+};
+
+export default Login;
