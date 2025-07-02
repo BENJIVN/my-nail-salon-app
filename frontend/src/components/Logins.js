3646fc49fc37d0242aa4b6936ffb106b0970b2d1
BENJIVN <bengi3288@gmail.com>
2025-07-02 01:41:37 -0400

    some basic login page

---
+++ frontend/src/components/Logins.js
@@ -0,0 +1,36 @@
+//all necessary elements for user authentication
+import React, {useState} from 'react';
+import {Form, Button, Container, Row, Col} from 'react-bootstrap';
+
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Login attempet with:', {email, password});
+        //Send a requst to the server here with tokens
+
+    }
+};
+
+return (
+    <Container>
+        <Row className='justify-content-md-center mt-5'>
+            <Col xs={12} md={6}>
+                <h2 className="text-center mb-4">Login</h2>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.label>Email address</Form.label>
+                        <Form.control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={(e) => setEmail(e.targert.value)}>
+                        </Form.control>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
+    </Container>
+)
